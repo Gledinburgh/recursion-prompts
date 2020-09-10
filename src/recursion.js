32778e999eb3e5b93bb08f128d8c570bee8f6672
Glenn Edinburgh <gledinburgh@gmail.com>
2020-09-09 20:08:26 -0500

    Created a function body for factorial, passing tests. Started on a function body for sum function.

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,25 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0 || n === 1) {
+    return 1
+  } else {
+    return n * factorial(n - 1);
+  }
+
+
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  // pass thorugh each number in an array
+  // the index of the array should decrease by 1
+  n = array[array.length - 1];
+  return array[n] + sum(array[n - 1]);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
